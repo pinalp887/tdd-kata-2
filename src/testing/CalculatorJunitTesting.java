9d577ff9ae6b0b58829cd740137f30a82ce48947
Pinal Sheradiya <pinal.sheradiya@cignexdatamatics.com>
2020-06-09 16:23:23 +0530

    added unit test cases

--- src/testing/CalculatorJunitTesting.java
+++ src/testing/CalculatorJunitTesting.java
@@ -7,27 +7,37 @@ public class CalculatorJunitTesting {
 
 	@Test
 	public void test() {
-		assertEquals(0.0, Operations.add(""), 0.1);
+		assertEquals(0.0, Operations.add(""), 0.2);
 	}
 
 	@Test
 	public void shouldReturnNumberOnNumber() {
-		assertEquals(1.0, Operations.add("1"), 0.1);
+		assertEquals(1.0, Operations.add("1"), 0.2);
 	}
 	
 	@Test
 	public void shouldReturnSumOnTwoNumbersDelimitedByComma() {
-		assertEquals(3.0, Operations.add("1,2"), 0.1);
+		assertEquals(3.0, Operations.add("1,2"), 0.2);
 	}
 	
 	@Test
 	public void shouldReturnSumOnMultipleNumbers() {
-		assertEquals(8.0, Operations.add("1,2,5"), 0.1);
+		assertEquals(8.0, Operations.add("1,2,5"), 0.2);
 	}
 	
 	@Test
 	public void shouldAcceptNewLineAsValidDelimiter() {
-		assertEquals(6.0, Operations.add("1,2\n3"), 0.1);
+		assertEquals(6.0, Operations.add("1,2\n3"), 0.2);
 	}
+	
+	@Test
+	public void shouldAdditionIfBothNumbersAreNegative() {
+		assertEquals(-9.00, Operations.add("-4,-5"), 0.2);
+	}
+	
+	@Test
+	public void shouldSubtractIfOneNumbersIsNegative() {
+		assertEquals(-2.00, Operations.add("-4,2"), 0.2);
+	} 
 
 }
